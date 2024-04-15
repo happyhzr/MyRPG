84775cdda2ddfda63446af41f36469bbabe876d1
happyhzr <insisthzr@163.com>
2024-04-15 22:11:16 +0800

    add a scriptable object

--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -4,4 +4,16 @@ using UnityEngine;
 
 public class Player : Character
 {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+            Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+            if (hitObject != null)
+            {
+                Debug.Log($"it: {hitObject.objectName}");
+                collision.gameObject.SetActive(false);
+            }
+        }
+    }
 }
